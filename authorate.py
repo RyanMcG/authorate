c4166e121cacdeba2ebcef103c290c1b7b5df8f6
Ryan McGowan <ryan@ryanmcg.com>
2012-11-25 20:04:33 -0500

    Start implementing load_books.
    Walks through paths creating a row for each one and finds all books
    under that path.

--- authorate.py
+++ authorate.py
@@ -18,6 +18,7 @@ from docopt import docopt, printable_usage
 from sqlalchemy import create_engine
 from model import create_db, get_session, Path
 import sys
+import os
 import re
 
 VERSION = "0.1.0-SNAPSHOT"
@@ -44,8 +45,41 @@ def display_error(e):
     print("ERROR: {error}\n\n{usage}".format(error=e, usage=USAGE_TEXT))
 
 
-def load_books(engine, author):
-    pass
+def filename_to_title(filename):
+    """For the given filename or path return the title of the book.
+
+    Typically, a filename is something like "TITLE - AUTHOR.EXTENSION". This
+    function assumes that pattern holds and extracts the TITLE portion.
+
+    >>> filename_to_title('David Copperfield - Charles Dickens.txt')
+    'David Copperfield'
+    >>> filename_to_title('Ugly - Title - Bad-Guy.mobi')
+    'Ugly - Title'
+    """
+    name, _ = os.path.splitext(os.path.basename(filename))
+    return TITLE_REGEX.match(name).groups()[0]
+
+
+def load_books(session, path, snippet_count=DEFAULT_SNIPPETS_COUNT, prefix='', verbose=False):
+    """For the given path create Path, Book and Snippet entries.
+
+    Each entry is put into the databse via the given session. Each run of load_books should create"""
+    if verbose:
+        print("Loading path: {path}".format(path=path))
+
+    # Create an instance of the given path in the database.
+    path_inst = Path(path, prefix)
+    session.add(path_inst)
+    session.commit()
+
+    book_paths = []
+    full_path = os.path.join(prefix, path)
+    for root, dirs, files in os.walk(full_path):
+        books = filter(BOOK_REGEX.match, files)
+        if len(books) > 0:
+            book_name = books[0]
+            title = filename_to_title(book_name)
+            book_paths.append(os.path.join(root, book_name))
 
 
 def authorate(arguments):
