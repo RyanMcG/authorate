2950df71e4fe11ce1d931bb00d4c2dd0599fbe7a
Chris Powers <canasclone@gmail.com>
2012-11-26 13:24:49 -0500

    Rename, refactor to allow multiple texts for same author

---
+++ author_style_extractor.py
@@ -0,0 +1,41 @@
+import nltk
+from nltk.probability import FreqDist
+
+class author_style_extractor:
+
+    def __init__(self):
+        self.tokens = []
+        self.fdist = FreqDist()
+
+    def add_text(self, text):
+        tokens = nltk.word_tokenize(text)
+        self.tokens += tokens
+        for token in tokens:
+            self.fdist.inc(token.lower())
+
+    def word_freq(self):
+        return self.fdist
+
+    def avg_word_length(self):
+        return sum([len(word) for word in self.tokens]) / float(self.fdist.N())
+
+    def max_word_length(self):
+        return max([len(word) for word in self.tokens])
+
+    def unique_word_freq(self):
+        return float(self.fdist.B()) / self.fdist.N()
+
+if __name__ == "__main__":
+    text1 = """Call me Ishmael.""" 
+    text2 = """Some years ago, never mind how long precisely,
+               having little or no money in my purse, and nothing
+               particular to interest me on shore, I thought I
+               would sail about a little and see the watery part
+               of the world."""
+    extr = author_style_extractor()
+    extr.add_text(text1)
+    extr.add_text(text2)
+    print(extr.word_freq())
+    print(extr.avg_word_length())
+    print(extr.max_word_length())
+    print(extr.unique_word_freq())
