a4f4135ab866c0e5efd8e08532347b306f0e4e21
Chris Powers <canasclone@gmail.com>
2012-11-26 21:22:32 -0500

    Add to vector methods

--- text_features.py
+++ text_features.py
@@ -1,11 +1,37 @@
 import nltk
 from nltk.probability import FreqDist
 
+def text_to_vector(text):
+    extr = TextFeatures()
+    extr.add_text(text)
+    return extr.to_vector()
+
 class TextFeatures:
 
     def __init__(self):
         self.tokens = []
         self.fdist = FreqDist()
+
+    def _word_freq_to_vector(self):
+        dist = self.word_freq()
+        most_common_words = ["the", "of", "to", "and", "a",
+                             "in", "is", "it", "you", "at"]
+        return [dist.freq(word) for word in most_common_words]
+
+    def _punctuation_freq_vector(self):
+        dist = self.word_freq()
+        punctuation = [".", ",", "!", "?", ";", ":"]
+        return [dist.freq(mark) for mark in punctuation]
+
+    def _word_length_freq_to_vector(self):
+        dist = self.word_length_freq()
+        return [dist.freq(length) for length in range(1,12)]
+
+    def _POS_freq_to_vector(self):
+        dist = self.POS_freq()
+        parts_of_speech = ["NN", "NNS", "NNP", "NNPS", "DT", "RB", "IN", "PRP",
+                           "CC", "VB", "VBD", "VBN", "JJ"]
+        return [dist.freq(pos) for pos in parts_of_speech]
 
     def add_text(self, text):
         tokens = nltk.word_tokenize(text)
@@ -14,7 +40,16 @@ class TextFeatures:
             self.fdist.inc(token.lower())
 
     def to_vector(self):
-        return []
+        return [
+            self.avg_word_length(),
+            float(self.std_dev_word_length()),
+            self.max_word_length(),
+            self.unique_word_freq(),
+        ]                                       \
+        + self._word_freq_to_vector()           \
+        + self._punctuation_freq_vector()       \
+        + self._word_length_freq_to_vector()    \
+        + self._POS_freq_to_vector()
 
     def word_freq(self):
         return self.fdist
@@ -54,10 +89,4 @@ if __name__ == "__main__":
     extr = TextFeatures()
     extr.add_text(text1)
     extr.add_text(text2)
-    print(extr.word_freq())
-    print(extr.word_length_freq())
-    print(extr.POS_freq())
-    print(extr.avg_word_length())
-    print(extr.std_dev_word_length())
-    print(extr.max_word_length())
-    print(extr.unique_word_freq())
+    print(extr.to_vector())
