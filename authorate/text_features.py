f3132a368de0edb763a3fc014049241914e1e145
Chris Powers <canasclone@gmail.com>
2012-12-07 00:09:20 -0500

    Add avg, std dev word commonality

--- authorate/text_features.py
+++ authorate/text_features.py
@@ -1,10 +1,12 @@
 import numpy, nltk
+from sqlalchemy import Column, Integer, String
 from nltk.probability import FreqDist, ConditionalFreqDist
 from itertools import tee, izip
+from authorate.model import WordCount
 
 
-def text_to_vector(text):
-    extr = TextFeatures()
+def text_to_vector(text, session):
+    extr = TextFeatures(session)
     extr.add_text(text)
     return extr.to_vector()
 
@@ -23,10 +25,14 @@ class TextFeatures:
                          "in", "is", "it", "you", "at"]
     punctuation = [".", ",", "!", "?", ";", ":"]
 
-    def __init__(self):
+    def __init__(self, session):
+        self.session = session
         self.tokens = []
         self.text = ""
         self.fdist = FreqDist()
+
+    def __get_word_commonality_counts(words):
+        return [session.query(WordCount).filter_by(word=w).first().count for w in words]
 
     def __sentence_lengths(self):
         "Return a list of the lengths of sentences"
@@ -73,6 +79,8 @@ class TextFeatures:
                  float(self.min_sentence_length()),
                  self.avg_sentence_length(),
                  self.std_sentence_length(),
+                 self.avg_word_commonality(),
+                 self.std_word_commonality(),
                  self.unique_word_freq()] +
                 self._word_freq_to_vector() +
                 self._punctuation_freq_vector() +
@@ -126,6 +134,14 @@ class TextFeatures:
     def std_sentence_length(self):
         return numpy.std(self.__sentence_lengths())
 
+    def avg_word_commonality(self):
+        counts = TextFeatures.__get_word_commonality_counts(self.text.split())
+        return numpy.avg(counts)
+
+    def std_word_commonality(self):
+        counts = TextFeatures.__get_word_commonality_counts(self.text.split())
+        return numpy.std(counts)
+
 
 if __name__ == "__main__":
     text1 = """Call me Ishmael."""
@@ -138,4 +154,3 @@ if __name__ == "__main__":
     extr.add_text(text1)
     extr.add_text(text2)
     print(extr.to_vector())
-    print(extr._POS_cond_freq_to_vector())
